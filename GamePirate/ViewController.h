1547f621880807099cbbeb5f51e95a6be805635f
Jagmeet <thalia@Thalias-iMac.local>
2015-02-05 12:40:58 -0800

    Initial Commit

---
+++ GamePirate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GamePirate
+//
+//  Created by Jagmeet on 2/5/15.
+//  Copyright (c) 2015 Think Ninja Cut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
