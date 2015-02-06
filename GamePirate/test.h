f0e03d4da7033fb6d281d1e6d9e506a5650ae718
jagjassam <jagjassam@gmail.com>
2015-02-06 00:33:17 -0800

    added a test file

---
+++ GamePirate/test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  GamePirate
+//
+//  Created by Jagmeet on 2/6/15.
+//  Copyright (c) 2015 Think Ninja Cut. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface test : NSObject
+
+@end
---
+++ GamePirate/test.m
@@ -0,0 +1,13 @@
+//
+//  test.m
+//  GamePirate
+//
+//  Created by Jagmeet on 2/6/15.
+//  Copyright (c) 2015 Think Ninja Cut. All rights reserved.
+//
+
+#import "test.h"
+
+@implementation test
+
+@end
